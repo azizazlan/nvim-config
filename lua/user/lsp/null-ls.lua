eeef379da9cf376b569c02850ebc48d625e3bd7c
Azlan <azlan@thuleen.io>
2022-08-27 12:32:51 +0800

    null-ls builtin prettier now add end semicolon and bracket spacing should have double dash

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -9,12 +9,11 @@ local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
 
 null_ls.setup({
-	debug = false,
+	debug = true,
 	sources = {
-		formatting.prettier.with({ extra_args = {"bracketSpacing : true", "--single-quote", "--jsx-single-quote" } }),
+		formatting.prettier.with({ extra_args = { "--single-quote", "--jsx-single-quote", "--bracketSpacing", "--semi" } }),
 		-- formatting.black.with({ extra_args = { "--fast" } }),
 		formatting.stylua,
-		formatting.rustfmt,
     -- diagnostics.flake8
 	},
 })
