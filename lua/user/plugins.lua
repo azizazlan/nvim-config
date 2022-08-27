10f2216a996ba8e2d5842d5f544e212327fdd501
Azlan <azlan@thuleen.io>
2022-08-27 12:03:29 +0800

    null-ls builtin prettier now add end semicolon and bracket spacing

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -70,7 +70,8 @@ return packer.startup(function(use)
   use("folke/which-key.nvim")
 
   -- Colorschemes
-  use("lunarvim/darkplus.nvim")
+  -- use("lunarvim/darkplus.nvim")
+  use("navarasu/onedark.nvim")
 
   -- cmp plugins
   use({ "hrsh7th/nvim-cmp", commit = "df6734aa018d6feb4d76ba6bda94b1aeac2b378a" }) -- The completion plugin
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -114,11 +114,6 @@ _G.packer_plugins = {
     path = "/Users/azlan/.local/share/nvim/site/pack/packer/start/cmp_luasnip",
     url = "https://github.com/saadparwaiz1/cmp_luasnip"
   },
-  ["darkplus.nvim"] = {
-    loaded = true,
-    path = "/Users/azlan/.local/share/nvim/site/pack/packer/start/darkplus.nvim",
-    url = "https://github.com/lunarvim/darkplus.nvim"
-  },
   ["friendly-snippets"] = {
     loaded = true,
     path = "/Users/azlan/.local/share/nvim/site/pack/packer/start/friendly-snippets",
@@ -201,6 +196,11 @@ _G.packer_plugins = {
     loaded = true,
     path = "/Users/azlan/.local/share/nvim/site/pack/packer/start/nvim-web-devicons",
     url = "https://github.com/kyazdani42/nvim-web-devicons"
+  },
+  ["onedark.nvim"] = {
+    loaded = true,
+    path = "/Users/azlan/.local/share/nvim/site/pack/packer/start/onedark.nvim",
+    url = "https://github.com/navarasu/onedark.nvim"
   },
   ["packer.nvim"] = {
     loaded = true,
